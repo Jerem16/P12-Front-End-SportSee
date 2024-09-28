7f07d21800be92fe07843010d7aa6aec3a4267c4
Jerem16 <jeremy.lemaignent@gmail.com>
2024-09-28 14:07:38 +0200

    Performance style end

--- src/components/01_Home/Resume_Activity/Performance/PerformanceCharts.jsx
+++ src/components/01_Home/Resume_Activity/Performance/PerformanceCharts.jsx
@@ -7,7 +7,7 @@ import {
     PolarAngleAxis,
     PolarRadiusAxis,
     Radar,
-    Tooltip,
+    RadialBar,
     ResponsiveContainer,
 } from "recharts";
 
@@ -21,8 +21,8 @@ const PerformanceCharts = () => {
             path={path}
             userId={userId}
             endPoint={endPoint}
-            // useMockData={true} 
-            // dataMocked={dataMocked}
+            useMockData={true}
+            dataMocked={dataMocked}
         >
             {(apiData) => {
                 // Vérifiez que les données sont bien présentes
@@ -53,27 +53,65 @@ const PerformanceCharts = () => {
                 console.log("formattedNewData", formattedNewData);
 
                 return (
-                    <>
-                        <ResponsiveContainer width="100%" height="100%">
-                            <RadarChart
-                                cx="50%"
-                                cy="50%"
-                                outerRadius="80%"
-                                data={formattedNewData}
-                            >
-                                <PolarGrid />
-                                <PolarAngleAxis dataKey="subject" />
-                                <PolarRadiusAxis />
-                                <Radar
-                                    name="Mike"
-                                    dataKey="value" // Utiliser 'value' car c'est l'attribut que vous affichez
-                                    stroke="#8884d8"
-                                    fill="#8884d8"
-                                    fillOpacity={0.6}
-                                />
-                            </RadarChart>
-                        </ResponsiveContainer>
-                    </>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <RadarChart
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={85}
+                            startAngle={90}
+                            data={formattedNewData.reverse()}
+                        >
+                            <PolarGrid radialLines={false} />
+                            <PolarAngleAxis
+                                dataKey="subject"
+                                axisLine={false}
+                                tickLine={false}
+                                tick={({ payload, x, y }) => {
+                                    // Ajustement de la position en fonction des coordonnées
+                                    let dx = 0;
+                                    let dy = 0;
+
+                                    if (x < 100) {
+                                        dx = -20; // Déplace le texte vers la gauche
+                                    } else if (x > 200) {
+                                        dx = 20; // Déplace le texte vers la droite
+                                    }
+
+                                    if (y < 40) {
+                                        dy = -10; // Texte en haut, déplacement légèrement vers le haut
+                                    } else if (y >= 40 && y <= 190) {
+                                        dy = 2.5; // Texte entre deux extrêmes (médiane), déplacement léger vers le bas
+                                    } else if (y > 200) {
+                                        dy = 15; // Texte en bas, déplacement important vers le bas
+                                    }
+
+                                    return (
+                                        <text
+                                            x={x}
+                                            y={y}
+                                            textAnchor="middle"
+                                            fill="#fff" // Couleur du texte
+                                            fontFamily="Roboto" // Police de caractères
+                                            fontSize="12px" // Taille de police
+                                            fontWeight="500"
+                                            dx={dx} // Applique le décalage horizontal
+                                            dy={dy} // Applique le décalage vertical
+                                        >
+                                            {payload.value}
+                                        </text>
+                                    );
+                                }}
+                            />
+                            <PolarRadiusAxis tick={false} axisLine={false} />
+                            <Radar
+                                name="Mike"
+                                dataKey="value"
+                                stroke=""
+                                fill="#FF0101"
+                                fillOpacity={0.6}
+                            />
+                        </RadarChart>
+                    </ResponsiveContainer>
                 );
             }}
         </FetchData>
